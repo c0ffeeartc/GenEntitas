734f01b71735fab5c9b860817c7df4073d6bbb0d
Yegor <c0ffeeartc@gmail.com>
2018-04-19 02:49:54 +0300

    + passing GenContextSystem

---
+++ Tests/Tests/GenContext/describe_GenContextSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using GenEntitas.Sources;
+using NSpec;
+
+namespace Tests.Tests
+{
+	public class describe_GenContextSystem : nspec
+	{
+		private				Contexts				_contexts;
+
+		private				void					given_empty_context		(  )
+		{
+			context["when adding context comp entity"] = (  ) =>
+			{
+				_contexts				= new Contexts();
+				var system				= new GenContextSystem( _contexts );
+
+				var ent					= _contexts.main.CreateEntity(  );
+				ent.AddContextComp( "Main" );
+
+				var ent2				= _contexts.main.CreateEntity(  );
+				ent2.AddContextComp( "Second" );
+
+				var contextGroup		= _contexts.main.GetGroup( MainMatcher.ContextComp );
+				var genFileGroup		= _contexts.main.GetGroup( MainMatcher.GeneratedFileComp );
+
+				it["has 0 generated file comps"] = (  ) =>
+				{
+					genFileGroup.count.should_be( 0 );
+				};
+
+				it["has contexts.Count generated file comps"] = (  ) =>
+				{
+					system.Execute(  );
+					genFileGroup.count.should_be( contextGroup.count );
+				};
+
+				it["Replaces markers in template"] = (  ) =>
+				{
+					var fileEnts = genFileGroup.GetEntities(  );
+					foreach ( var fileEnt in fileEnts )
+					{
+						fileEnt.generatedFileComp.Contents.IndexOf( '$' ).should_be( -1 );
+						//Console.Write( fileEnt.generatedFileComp.Contents );
+					}
+				};
+			};
+		}
+	}
+}
