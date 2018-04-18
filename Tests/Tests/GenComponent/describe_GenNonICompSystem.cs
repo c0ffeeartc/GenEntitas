69cf097511608b7ab5239ef79cbd095b15768fca
Yegor <c0ffeeartc@gmail.com>
2018-04-19 01:38:39 +0300

    +! passing GenNonICompSystem

---
+++ Tests/Tests/GenComponent/describe_GenNonICompSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using GenEntitas.Sources;
+using NSpec;
+
+namespace Tests.Tests
+{
+	public class describe_GenNonICompSystem : nspec
+	{
+		private				Contexts				_contexts;
+
+		private				void					given_empty_context		(  )
+		{
+			context["when adding context comp entity"] = (  ) =>
+			{
+				_contexts				= new Contexts();
+				var system				= new GenNonICompSystem( _contexts );
+
+				var ent					= _contexts.main.CreateEntity(  );
+				ent.AddNonIComp( "TestComp1", "TestComp1" );
+
+				var genFileGroup		= _contexts.main.GetGroup( MainMatcher.GeneratedFileComp );
+
+				it["has 0 generated file comps"] = (  ) =>
+				{
+					genFileGroup.count.should_be( 0 );
+				};
+
+				it["has 1 generated file comps"] = (  ) =>
+				{
+					system.Execute(  );
+					genFileGroup.count.should_be( 1 );
+				};
+
+				it["Replaces markers in template"] = (  ) =>
+				{
+					var fileEnts = genFileGroup.GetEntities(  );
+					foreach ( var fileEnt in fileEnts )
+					{
+						fileEnt.generatedFileComp.Contents.IndexOf( '$' ).should_be( -1 );
+					}
+				};
+			};
+		}
+	}
+}
