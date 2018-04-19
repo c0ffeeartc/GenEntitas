3e6cbf617ae3552e75179ea59b6e27f5eb5bce94
Yegor <c0ffeeartc@gmail.com>
2018-04-19 03:13:21 +0300

    + passing GenContextsObserverSystem

---
+++ Tests/Tests/GenContexts/describe_GenContextsObserverSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using GenEntitas.Sources;
+using NSpec;
+
+namespace Tests.Tests
+{
+	public class describe_GenContextsObserverSystem : nspec
+	{
+		private				Contexts				_contexts;
+
+		private				void					given_empty_context		(  )
+		{
+			context["when adding context comp entity"] = (  ) =>
+			{
+				_contexts				= new Contexts(  );
+				var system				= new GenContextsObserverSystem( _contexts );
+
+				var ent					= _contexts.main.CreateEntity(  );
+				ent.AddContextComp( "Main" );
+
+				var ent2				= _contexts.main.CreateEntity(  );
+				ent2.AddContextComp( "Second" );
+
+				var genFileGroup		= _contexts.main.GetGroup( MainMatcher.GeneratedFileComp );
+
+				it["has 0 generated file comps"] = (  ) =>
+				{
+					genFileGroup.count.should_be( 0 );
+				};
+
+				it["has 1 generated file comps"] = (  ) =>
+				{
+					system.Execute(  );
+					genFileGroup.count.should_be( 1 );
+				};
+
+				it["Replaces markers in template"] = (  ) =>
+				{
+					var fileEnt		= genFileGroup.GetSingleEntity(  );
+					fileEnt.generatedFileComp.Contents.IndexOf( '$' ).should_be( -1 );
+					//Console.Write( fileEnt.generatedFileComp.Contents );
+				};
+			};
+		}
+	}
+}
