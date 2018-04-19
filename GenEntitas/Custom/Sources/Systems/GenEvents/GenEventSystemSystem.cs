61dce39b0231652afa5c910a73132e5ed74482c0
Yegor <c0ffeeartc@gmail.com>
2018-04-20 00:51:13 +0300

    ! passing GenEventSystemSystem

--- GenEntitas/Custom/Sources/Systems/GenEvents/GenEventSystemSystem.cs
+++ GenEntitas/Custom/Sources/Systems/GenEvents/GenEventSystemSystem.cs
@@ -1,7 +1,196 @@
-﻿namespace GenEntitas.Sources
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DesperateDevs.Utils;
+using Entitas;
+using Entitas.CodeGeneration.Attributes;
+using Entitas.CodeGeneration.Plugins;
+using Ent = MainEntity;
+
+namespace GenEntitas.Sources
 {
-	public class GenEventSystemSystem
+	public class GenEventSystemSystem : ReactiveSystem<Ent>
 	{
-		
+		public				GenEventSystemSystem	( Contexts contexts ) : base( contexts.main )
+		{
+			_contexts			= contexts;
+		}
+
+		private				Contexts				_contexts;
+		private const		String					TEMPLATE				=
+@"public sealed class ${Event}EventSystem : Entitas.ReactiveSystem<${EntityType}> {
+
+    readonly Entitas.IGroup<${EntityType}> _listeners;
+
+    public ${Event}EventSystem(Contexts contexts) : base(contexts.${contextName}) {
+        _listeners = contexts.${contextName}.GetGroup(${MatcherType}.${EventListener});
+    }
+
+    protected override Entitas.ICollector<${EntityType}> GetTrigger(Entitas.IContext<${EntityType}> context) {
+        return Entitas.CollectorContextExtension.CreateCollector(
+            context, Entitas.TriggerOnEventMatcherExtension.${GroupEvent}(${MatcherType}.${ComponentName})
+        );
+    }
+
+    protected override bool Filter(${EntityType} entity) {
+        return ${filter};
+    }
+
+    protected override void Execute(System.Collections.Generic.List<${EntityType}> entities) {
+        foreach (var e in entities) {
+            ${cachedAccess}
+            foreach (var listenerEntity in _listeners) {
+                foreach (var listener in listenerEntity.${eventListener}.value) {
+                    listener.On${ComponentName}${EventType}(e${methodArgs});
+                }
+            }
+        }
+    }
+}
+";
+
+		private const		String				BIND_TO_ENTITY_TEMPLATE		=
+@"public sealed class ${Event}EventSystem : Entitas.ReactiveSystem<${EntityType}> {
+
+    public ${Event}EventSystem(Contexts contexts) : base(contexts.${contextName}) {
+    }
+
+    protected override Entitas.ICollector<${EntityType}> GetTrigger(Entitas.IContext<${EntityType}> context) {
+        return Entitas.CollectorContextExtension.CreateCollector(
+            context, Entitas.TriggerOnEventMatcherExtension.${GroupEvent}(${MatcherType}.${ComponentName})
+        );
+    }
+
+    protected override bool Filter(${EntityType} entity) {
+        return ${filter};
+    }
+
+    protected override void Execute(System.Collections.Generic.List<${EntityType}> entities) {
+        foreach (var e in entities) {
+            ${cachedAccess}
+            foreach (var listener in e.${eventListener}.value) {
+                listener.On${ComponentName}${EventType}(e${methodArgs});
+            }
+        }
+    }
+}
+";
+
+		protected override	ICollector<Ent>			GetTrigger				( IContext<Ent> context )
+		{
+			return context.CreateCollector( MainMatcher.AllOf( MainMatcher.Comp, MainMatcher.EventComp ) );
+		}
+
+		protected override	Boolean					Filter					( Ent entity )
+		{
+			return entity.hasComp && entity.hasEventComp;
+		}
+
+		protected override	void					Execute					( List<Ent> entities )
+		{
+			var contextEnts = new Dictionary<String, List<MainEntity>>(  );
+			foreach ( var ent in entities )
+			{
+				foreach ( var contextName in ent.contextNamesComp.Values )
+				{
+					if ( !contextEnts.ContainsKey( contextName ) )
+					{
+						contextEnts[contextName] = new List<MainEntity>( );
+					}
+					contextEnts[contextName].Add( ent );
+				}
+			}
+
+			foreach ( var ent in entities )
+			{
+				foreach ( var contextName in contextEnts.Keys )
+				{
+					foreach ( var eventInfo in ent.eventComp.Values )
+					{
+						CreateFileEnt( ent, eventInfo, contextName );
+					}
+				}
+			}
+		}
+
+		private void CreateFileEnt( MainEntity ent, EventInfo eventInfo, string contextName )
+		{
+			var methodArgs = ent.GetEventMethodArgs( eventInfo, ", " + ( !ent.hasPublicFieldsComp
+				? ent.PrefixedComponentName( )
+				: GetMethodArgs( ent.publicFieldsComp.Values.ToArray( ) ) ) );
+
+			var filePath = "Events" + Path.DirectorySeparatorChar + "Systems" + Path.DirectorySeparatorChar +
+			               ent.Event( contextName, eventInfo ) + "EventSystem.cs";
+
+			var template = eventInfo.BindToEntity
+				? BIND_TO_ENTITY_TEMPLATE
+				: TEMPLATE;
+
+			var cachedAccess = !ent.hasPublicFieldsComp
+				? string.Empty
+				: "var component = e." + ent.ComponentName( ).LowercaseFirst( ) + ";";
+
+			if ( eventInfo.EventType == EventType.Removed )
+			{
+				methodArgs = string.Empty;
+				cachedAccess = string.Empty;
+			}
+
+			var contents = template
+				.Replace( "${GroupEvent}", eventInfo.EventType.ToString( ) )
+				.Replace( "${filter}", GetFilter( ent, contextName, eventInfo ) )
+				.Replace( "${cachedAccess}", cachedAccess )
+				.Replace( "${methodArgs}", methodArgs )
+				.Replace( ent, contextName, eventInfo );
+
+			var generatedBy = GetType( ).FullName;
+
+			var fileEnt = _contexts.main.CreateEntity( );
+			fileEnt.AddGeneratedFileComp( filePath, contents, generatedBy );
+		}
+
+		private				String					GetFilter				( MainEntity ent, string contextName, EventInfo eventInfo )
+		{
+			var filter			= string.Empty;
+			if (!ent.hasPublicFieldsComp)
+			{
+				switch (eventInfo.EventType)
+				{
+					case EventType.Added:
+						filter = "entity." + ent.PrefixedComponentName();
+						break;
+					case EventType.Removed:
+						filter = "!entity." + ent.PrefixedComponentName();
+						break;
+				}
+			}
+			else
+			{
+				switch (eventInfo.EventType)
+				{
+					case EventType.Added:
+						filter = "entity.has" + ent.ComponentName();
+						break;
+					case EventType.Removed:
+						filter = "!entity.has" + ent.ComponentName();
+						break;
+				}
+			}
+
+			if (eventInfo.BindToEntity) {
+				filter += " && entity.has" + ent.EventListener(contextName, eventInfo);
+			}
+
+			return filter;
+		}
+
+		private				String					GetMethodArgs			(FieldInfo[] memberData)
+		{
+			return string.Join(", ", memberData
+				.Select(info => "component." + info.FieldName)
+				.ToArray()
+			);
+		}
 	}
 }
--- Tests/Tests/GenEvents/describe_GenEventSystemSystem.cs
+++ Tests/Tests/GenEvents/describe_GenEventSystemSystem.cs
@@ -1,9 +1,63 @@
-﻿using NSpec;
+﻿using System;
+using System.Collections.Generic;
+using Entitas.CodeGeneration.Attributes;
+using GenEntitas.Sources;
+using NSpec;
 
 namespace Tests.Tests
 {
 	public class describe_GenEventSystemSystem : nspec
 	{
-		
+		private				Contexts				_contexts;
+
+		private				void					given_empty_context		(  )
+		{
+			context["when adding context comp entity"] = (  ) =>
+			{
+				_contexts				= new Contexts(  );
+				var system				= new GenEventSystemSystem( _contexts );
+
+				var ent					= _contexts.main.CreateEntity(  );
+				ent.AddComp( "TestComp1", "TestComp1" );
+				ent.AddEventComp( new List<EventInfo>(  )
+					{
+						new EventInfo{BindToEntity = true, EventType = EventType.Added, Priority =  0},
+						new EventInfo{BindToEntity = false, EventType = EventType.Removed, Priority =  0},
+					} );
+				ent.AddContextNamesComp( new List<string>{ "Main", "Second" });
+
+//				var ent2					= _contexts.main.CreateEntity(  );
+//				ent2.AddComp( "TestComp2", "TestComp2" );
+//				ent2.AddEventComp( new List<EventInfo>(  )
+//					{
+//						new EventInfo{BindToEntity = true, EventType = EventType.Added, Priority =  0},
+//						new EventInfo{BindToEntity = false, EventType = EventType.Removed, Priority =  0},
+//					} );
+//				ent2.AddContextNamesComp( new List<string>{ "Main", "Second" });
+
+				var genFileGroup		= _contexts.main.GetGroup( MainMatcher.GeneratedFileComp );
+
+				it["has 0 generated file comps"] = (  ) =>
+				{
+					genFileGroup.count.should_be( 0 );
+				};
+
+				it["has contexts.Count * eventInfo.Count generated file comps"] = (  ) =>
+				{
+					system.Execute(  );
+					genFileGroup.count.should_be( ent.contextNamesComp.Values.Count * ent.eventComp.Values.Count );
+				};
+
+				it["Replaces markers in template"] = (  ) =>
+				{
+					var fileEnts = genFileGroup.GetEntities(  );
+					foreach ( var fileEnt in fileEnts )
+					{
+						fileEnt.generatedFileComp.Contents.IndexOf( '$' ).should_be( -1 );
+						//Console.Write( fileEnt.generatedFileComp.Contents );
+					}
+				};
+			};
+		}
 	}
 }
