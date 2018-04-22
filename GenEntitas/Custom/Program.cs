735be306de7f1c0f2570e87d25d026c95ceaad61
Yegor <c0ffeeartc@gmail.com>
2018-04-22 03:48:56 +0300

    ! dryMode and generate path in commandline args

--- GenEntitas/Custom/Program.cs
+++ GenEntitas/Custom/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using CommandLine;
 
@@ -8,6 +9,12 @@ namespace GenEntitas
 	{
 		[Option( "dllPaths", Required = true, HelpText = "Reflection dlls with Component classes" )]
 		public String DllPaths { get; set; }
+
+		[Option( "generatePath", Required = true, HelpText = "Where to place Generated folder. WARNING: removes and creates Generated folder on each run" )]
+		public String GeneratePath { get; set; }
+
+		[Option( "dryMode", HelpText = "Do not remove or write anything on disk. WARNING: plugin devs should handle this option" )]
+		public Boolean RunInDryMode { get; set; }
 	}
 
 	internal class Program
@@ -18,6 +25,12 @@ namespace GenEntitas
 		}
 		private static void WithParsedArgs( Options options )
 		{
+			if ( !Directory.Exists( options.GeneratePath ) )
+			{
+				Console.WriteLine( $"Generate path does not exist: '{options.GeneratePath}'" );
+				return;
+			}
+
 			var runner = new Runner();
 			runner.Init(  );
 
@@ -33,7 +46,8 @@ namespace GenEntitas
 		private static void FillContexts( Contexts contexts, Options options )
 		{
 			contexts.settings.isConsoleWriteLineGeneratedPaths		= true;
-			contexts.settings.SetGeneratePath( "./" );
+			contexts.settings.isRunInDryMode						= options.RunInDryMode;
+			contexts.settings.SetGeneratePath( String.IsNullOrEmpty( options.GeneratePath ) ? "./" : options.GeneratePath );
 			contexts.settings.SetReflectionAssemblyPaths( options.DllPaths.Split(',').ToList(  ) );
 		}
 	}
