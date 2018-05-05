677fdde9050fe5df0917c181d25fd66d9a2f2a95
Yegor <c0ffeeartc@gmail.com>
2018-05-05 13:54:02 +0300

    +! move systems into GenentiasLib project

--- genComps.sh
+++ genComps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
-mono ./GenEntitas/bin/Debug/GenEntitas.exe --dllPaths="./ComponentsLib/bin/Debug/ComponentsLib.dll" --generatePath="./GenEntitas/Custom/Sources"
+mono ./GenEntitas/bin/Debug/GenEntitas.exe --dllPaths="./ComponentsLib/bin/Debug/ComponentsLib.dll" --generatePath="./GenEntitasLib/Custom/Sources"
