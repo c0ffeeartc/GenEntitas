4894bedeb2b3da750b7b3c7022b53715a9de250d
Yegor <c0ffeeartc@gmail.com>
2018-05-22 02:30:09 +0300

    ! don't run tests when build errors

--- tests.sh
+++ tests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 clear
-xbuild /property:Configuration=Release /verbosity:minimal CodeGen.sln
-mono Tests/bin/Release/Tests.exe $@
+xbuild /property:Configuration=Release /verbosity:minimal CodeGen.sln && mono Tests/bin/Release/Tests.exe $@
