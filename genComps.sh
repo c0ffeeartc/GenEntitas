2f898844545068c16b7242ab7abeacb64baf9251
Yegor <c0ffeeartc@gmail.com>
2018-11-22 19:33:41 +0200

    ~ rename and move ConsoleRunner project

--- genComps.sh
+++ genComps.sh
@@ -2,4 +2,4 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
 #mono ./GenEntitas/bin/Debug/GenEntitas.exe --dllPaths="./ComponentsLib/bin/Debug/ComponentsLib.dll" --generatePath="./Generated/Sources" --generatedNamespace="GenEntitas"
-mono ./GenEntitas/bin/Debug/GenEntitas.exe --SettingsPath=./GenComps.settings
+mono ./GenEntitas.Runner.Console/bin/Debug/GenEntitas.exe --SettingsPath=./GenComps.settings
