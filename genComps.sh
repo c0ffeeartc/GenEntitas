e428defdff0d5529d7af0479762f27e5249ec7e5
Yegor <c0ffeeartc@gmail.com>
2018-11-30 15:56:16 +0200

    ~

--- genComps.sh
+++ genComps.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
-#mono ./GenEntitas/bin/Debug/GenEntitas.exe --dllPaths="./ComponentsLib/bin/Debug/ComponentsLib.dll" --generatePath="./Generated/Sources" --generatedNamespace="GenEntitas"
 mono ./GenEntitas.Runner.Console/bin/Debug/GenEntitas.exe --SettingsPath=./GenComps.settings
