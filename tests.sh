68b32c2d06e0e0155c0ebea7f40e4b81d184a965
Yegor <c0ffeeartc@gmail.com>
2018-12-14 20:36:57 +0200

    change travis and tests

--- tests.sh
+++ tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 clear
-msbuild /property:Configuration=Release /verbosity:minimal CodeGen.sln && mono Tests/bin/Release/Tests.exe $@
+#msbuild /property:Configuration=Release /verbosity:minimal CodeGen.sln
+mono Tests/bin/Release/Tests.exe $@
