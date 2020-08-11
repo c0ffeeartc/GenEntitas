88398d66f5da029a875e85edcf55adad13584d57
c0ffeeartc <c0ffeeartc@gmail.com>
2020-08-12 01:04:55 +0300

    ! update packages
    it was important to keep "System.Text.Encoding.CodePages" at version="4.5.1", otherwise project.GetCompilationAsync had exception for some projects

--- GenEntitas.Core.Systems/Sources/Systems/CodeGenerators/GenComponent/GenComponentSystem.cs
+++ GenEntitas.Core.Systems/Sources/Systems/CodeGenerators/GenComponent/GenComponentSystem.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.Composition;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using DesperateDevs.Utils;
 using Entitas;
 using Ent = GenEntitas.MainEntity;
 
